c56aeab92fa2504722e03476fcf79e47df57729d
Boris Dalstein <dalboris@gmail.com>
2022-04-04 17:39:51 +0200

    Add freetype dep (#2)
    * Install VCPKG deps
    
    * Add Freetype
    
    * Add vgpkg toolchain file
    
    * Remove manifest mode off
    
    * use ilammy action, manifest mode on
    
    * manifest install off
    
    * test
    
    * deferred vcpkg install

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+FT_Library library;
+
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	FT_Error error = FT_Init_FreeType(&library);
+	if (error) {
+		std::cout << "An error occured!" << std::endl;
+	}
+	else {
+		std::cout << "No error" << std::endl;
+	}
 	return 0;
 }
